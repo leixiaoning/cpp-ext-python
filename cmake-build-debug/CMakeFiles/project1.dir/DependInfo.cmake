58023be6011905b18f83a132df8ca8eab36c9ad4
“leixiaoning” <794377332@qq.com>
2021-03-09 14:59:39 +0800

    t co

---
+++ cmake-build-debug/CMakeFiles/project1.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/shanmu/CLionProjects/project1/cpp4pymeanshift/RAList.cpp" "/Users/shanmu/CLionProjects/project1/cmake-build-debug/CMakeFiles/project1.dir/cpp4pymeanshift/RAList.cpp.o"
+  "/Users/shanmu/CLionProjects/project1/cpp4pymeanshift/ms.cpp" "/Users/shanmu/CLionProjects/project1/cmake-build-debug/CMakeFiles/project1.dir/cpp4pymeanshift/ms.cpp.o"
+  "/Users/shanmu/CLionProjects/project1/cpp4pymeanshift/msImageProcessor.cpp" "/Users/shanmu/CLionProjects/project1/cmake-build-debug/CMakeFiles/project1.dir/cpp4pymeanshift/msImageProcessor.cpp.o"
+  "/Users/shanmu/CLionProjects/project1/cpp4pymeanshift/rlist.cpp" "/Users/shanmu/CLionProjects/project1/cmake-build-debug/CMakeFiles/project1.dir/cpp4pymeanshift/rlist.cpp.o"
+  "/Users/shanmu/CLionProjects/project1/main.cpp" "/Users/shanmu/CLionProjects/project1/cmake-build-debug/CMakeFiles/project1.dir/main.cpp.o"
+  "/Users/shanmu/CLionProjects/project1/math1.cpp" "/Users/shanmu/CLionProjects/project1/cmake-build-debug/CMakeFiles/project1.dir/math1.cpp.o"
+  "/Users/shanmu/CLionProjects/project1/someOPs/math2.cpp" "/Users/shanmu/CLionProjects/project1/cmake-build-debug/CMakeFiles/project1.dir/someOPs/math2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_FOUND"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Frameworks/Python.framework/Versions/3.9/include/python3.9"
+  "/usr/local/Cellar/opencv/4.5.1_3/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ someOPs/math2.h
@@ -0,0 +1,17 @@
+//
+// Created by 山姆 on 2021/3/7.
+//
+
+#ifndef PROJECT1_MATH2_H
+#define PROJECT1_MATH2_H
+#include <Python.h>
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+//#include </usr/local/lib/python3.9/site-packages/numpy/core/include/numpy/arrayobject.h>
+
+int f3();
+PyObject* MatToArray(Mat src);
+
+#endif //PROJECT1_MATH2_H
+
+
